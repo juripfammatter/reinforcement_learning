aaa6de87f6d22dbb5c97e48c48ec946a0bb97b86
juripfammatter <juri.pfammatter@gmail.com>
2024-03-31 18:58:03 +0200

    updated model

--- pytorch/intro_example_train.py
+++ pytorch/intro_example_train.py
@@ -86,7 +86,6 @@ def test(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-
 """ Running the training and test loops """
 
 epochs = 40
